b4aa7f15f93263ee65ce5897c16137ee5ec3483b
Ahmad Firdaus <firdaus1337@gmail.com>
2021-03-18 12:47:47 +0800

    this is the third commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,15 @@
+package samples.demo;
+
+public class Location {
+
+	private String location;
+	
+	void setLocation(String location) {
+	this.location = location;	
+	}
+		
+	String getLocation()
+	{
+		return location;
+	}
+}
